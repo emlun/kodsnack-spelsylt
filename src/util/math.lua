85092b301adaea7573589960465dfab4c40edb52
Emil Lundberg <emil@emlun.se>
2018-11-14 02:54:37 +0100

    Add function util.math.isFinite

--- src/util/math.lua
+++ src/util/math.lua
@@ -1,10 +1,15 @@
 local util = require("util.table")
 
 
+local function isFinite(a)
+  return a < math.huge and a > -math.huge
+end
+
 local function lerp(from, to, scale)
   return from + (to - from) * scale
 end
 
 return util.readonlytable({
+  isFinite = isFinite,
   lerp = lerp,
 })
