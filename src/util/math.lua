4860caaaf0bdeb6acc4c4cd31a0b9b78ca8dcb02
Emil Lundberg <emil@emlun.se>
2018-11-15 00:14:20 +0100

    Remove util.math.lerp()

--- src/util/math.lua
+++ src/util/math.lua
@@ -5,11 +5,6 @@ local function isFinite(a)
   return a < math.huge and a > -math.huge
 end
 
-local function lerp(from, to, scale)
-  return from + (to - from) * scale
-end
-
 return util.readonlytable({
   isFinite = isFinite,
-  lerp = lerp,
 })
