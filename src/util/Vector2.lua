928f19c47a8faf38dca6c68f67f6866f063a8efe
Emil Lundberg <emil@emlun.se>
2018-11-17 02:22:52 +0100

    Don't use readonlytable for Vector2 module

--- src/util/Vector2.lua
+++ src/util/Vector2.lua
@@ -1,42 +1,4 @@
-local new
-
-local function add(v1, v2)
-  return new(v1.x + v2.x, v1.y + v2.y)
-end
-
-local function sub(v1, v2)
-  return new(v1.x - v2.x, v1.y - v2.y)
-end
-
-local function neg(v)
-  return new(-v.x, -v.y)
-end
-
-local function mul(lhs, rhs)
-  if "number" == type(lhs) then
-    return new(lhs * rhs.x, lhs * rhs.y)
-  elseif "number" == type(rhs) then
-    return new(rhs * lhs.x, rhs * lhs.y)
-  else
-    error("Exactly one parameter must be a number, was: " .. type(lhs) .. " and " .. type(rhs))
-  end
-end
-
-local function eq(v1, v2)
-  return v1.x == v2.x and v1.y == v2.y
-end
-
-local function normalized(v)
-  if v.x == 0 and v.y == 0 then
-    return v
-  else
-    return v * (1 / v:mag())
-  end
-end
-
-local function mag(v)
-  return math.sqrt(v.x^2 + v.y^2)
-end
+local Vector2 = {}
 
 local function vector_tostring(self)
   return "Vector2(" .. self.x .. ", " .. self.y .. ")"
@@ -46,21 +8,21 @@ local function newindex(self, key, value)
   error("Attempted to set [" .. tostring(key) .. "] = [" .. tostring(value) .. "] in " .. tostring(self))
 end
 
-new = function (x, y)
+function Vector2.new (x, y)
   return setmetatable(
     {},
     {
       __index = {
         x = x,
         y = y,
-        normalized = normalized,
-        mag = mag,
+        normalized = Vector2.normalized,
+        mag = Vector2.mag,
       },
-      __unm = neg,
-      __add = add,
-      __sub = sub,
-      __mul = mul,
-      __eq = eq,
+      __unm = Vector2.neg,
+      __add = Vector2.add,
+      __sub = Vector2.sub,
+      __mul = Vector2.mul,
+      __eq = Vector2.eq,
       __metatable = false,
       __newindex = newindex,
       __tostring = vector_tostring,
@@ -68,15 +30,50 @@ new = function (x, y)
   )
 end
 
+function Vector2.add (v1, v2)
+  return Vector2.new(v1.x + v2.x, v1.y + v2.y)
+end
+
+function Vector2.sub (v1, v2)
+  return Vector2.new(v1.x - v2.x, v1.y - v2.y)
+end
+
+function Vector2.neg (v)
+  return Vector2.new(-v.x, -v.y)
+end
+
+function Vector2.mul (lhs, rhs)
+  if "number" == type(lhs) then
+    return Vector2.new(lhs * rhs.x, lhs * rhs.y)
+  elseif "number" == type(rhs) then
+    return Vector2.new(rhs * lhs.x, rhs * lhs.y)
+  else
+    error("Exactly one parameter must be a number, was: " .. type(lhs) .. " and " .. type(rhs))
+  end
+end
+
+function Vector2.eq (v1, v2)
+  return v1.x == v2.x and v1.y == v2.y
+end
+
+function Vector2.normalized (v)
+  if v.x == 0 and v.y == 0 then
+    return v
+  else
+    return v * (1 / v:mag())
+  end
+end
+
+function Vector2.mag (v)
+  return math.sqrt(v.x^2 + v.y^2)
+end
+
 return setmetatable(
-  {},
+  Vector2,
   {
-    __call = function(_, x, y) return new(x, y) end,
+    __call = function(_, x, y) return Vector2.new(x, y) end,
     __index = {
-      zero = new(0, 0),
+      zero = Vector2.new(0, 0),
     },
-    __newindex = function()
-      error("Cannot modify Vector2 module.")
-    end,
   }
 )
