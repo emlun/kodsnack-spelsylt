b04cbec043773c2e82de51acfc365445df7fa59b
Emil Lundberg <emil@emlun.se>
2018-11-18 23:29:26 +0100

    Add method ResourceBar.get_height

--- src/hud/ResourceBar.lua
+++ src/hud/ResourceBar.lua
@@ -22,6 +22,10 @@ local Vector2 = require("util.Vector2")
 local ResourceBar = {}
 local ResourceBar_mt = { __index = ResourceBar }
 
+
+ResourceBar.font = love.graphics.newFont(12)
+ResourceBar.padding = { top = 5, right = 7, bottom = 5, left = 7 }
+
 function ResourceBar.new (resource, w, h, args)
   args = args or {}
 
@@ -50,17 +54,17 @@ function ResourceBar.update (self)
   return self
 end
 
+function ResourceBar.get_height (self)
+  return self.show_text
+    and (self.font:getHeight() + self.padding.top + self.padding.bottom)
+    or self.h
+end
+
 function ResourceBar.draw (self, origin_x, origin_y, _, _, opacity)
   opacity = opacity or 1
   local fill_width = self.w * self.resource:check() / self.resource.capacity
 
-  local font = love.graphics.newFont(12)
-
-  local padding = { top = 5, right = 7, bottom = 5, left = 7 }
-
-  local h = self.show_text
-    and (font:getHeight() + padding.top + padding.bottom)
-    or self.h
+  local h = self:get_height()
 
   love.graphics.setColor(self:get_fill_color(opacity))
   love.graphics.rectangle("fill", origin_x, origin_y, fill_width, h)
@@ -68,7 +72,7 @@ function ResourceBar.draw (self, origin_x, origin_y, _, _, opacity)
   love.graphics.rectangle("line", origin_x, origin_y, self.w, h)
 
   if self.show_text then
-    local text_pos = Vector2(self.w - padding.right, padding.top)
+    local text_pos = Vector2(self.w - self.padding.right, self.padding.top)
     local text
     if self.resource.short_name then
       text = string.format("%d %s", self.resource:check(), self.resource.short_name:get())
@@ -76,10 +80,10 @@ function ResourceBar.draw (self, origin_x, origin_y, _, _, opacity)
       text = string.format("%d", self.resource:check())
     end
 
-    local text_graphic = love.graphics.newText(font, text)
-    local label_graphic = love.graphics.newText(font, self.label:get_capitalized())
+    local text_graphic = love.graphics.newText(self.font, text)
+    local label_graphic = love.graphics.newText(self.font, self.label:get_capitalized())
     love.graphics.setColor(1, 1, 1, 1 * opacity)
-    love.graphics.draw(label_graphic, origin_x + padding.left, origin_y + padding.top)
+    love.graphics.draw(label_graphic, origin_x + self.padding.left, origin_y + self.padding.top)
     love.graphics.draw(text_graphic, origin_x + text_pos.x - text_graphic:getWidth(), origin_y + text_pos.y)
   end
 end
