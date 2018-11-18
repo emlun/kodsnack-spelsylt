37dd40f71037c87f1aa9d096dd8e44e4a8a023b8
Emil Lundberg <emil@emlun.se>
2018-11-18 05:46:43 +0100

    Expand sandbox map

--- src/resources/tiles/Super Metroid.tsx
+++ src/resources/tiles/Super Metroid.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Super Metroid" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="3136" columns="55">
+<tileset version="1.2" tiledversion="1.2.1" name="Super Metroid" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="3025" columns="55">
  <image source="super-metroid.png" width="950" height="950"/>
 </tileset>
