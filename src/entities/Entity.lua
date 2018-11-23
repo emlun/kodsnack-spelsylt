293d726f0d268dae36daa61b057e5bf88e91b86f
Emil Lundberg <emil@emlun.se>
2018-11-23 01:22:03 +0100

    De-lint Entity

--- src/entities/Entity.lua
+++ src/entities/Entity.lua
@@ -14,6 +14,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+-- luacheck: globals love
+
 local Vector2 = require("util.Vector2")
 local mydebug = require("src.debug")
 
